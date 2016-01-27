bc4a4d389590008bdfa68ced2f22addcec4e9ee5
Mael Thomas <mael.thomas@data.gouv.fr>
2016-01-27 10:42:41 +0100

    Yo

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app     = express();
+var githubMiddleware = require('github-webhook-middleware')({
+	secret: 'inconnu'
+});
+
+app.post('/hooks/github/', githubMiddleware, function(req, res) {
+	// Only respond to github push events
+	if (req.headers['x-github-event'] != 'push') return res.status(200).end();
+
+	var payload = req.body
+		, repo    = payload.repository.full_name
+		, branch  = payload.ref.split('/').pop();
+
+  console.log(repo, branch)
+
+  res.status(200).end();
+
+});
+
+app.listen(7777, function () {
+  console.log('Oui ?');
+});
